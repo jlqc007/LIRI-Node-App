6643a5f6408ab0517bc602c7e6dc825e545db27a
jlqc007 <jefflutz76@gmail.com>
2017-08-24 18:04:31 -0400

    Final

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log("this is loaded");
+
+exports.twitterKeys = {
+    consumer_key: '3HcdFmLMqxGaUMfABYQULY3Qa',
+    consumer_secret: 'NFWL3lAVRxrUfULYGU9fYvt0beZuHSgUl1hgUvg84FyHqhjGlF',
+    access_token_key: '898246450618015744-4uNrQsTHyhQDu3AHamCBfBBqvV8HzqY',
+    access_token_secret: '4wf9sGkddNONeppPlnCHXxnsdgAL34PqVeiSuvQ7BsUhZ',
+}
